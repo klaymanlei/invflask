8221f16208b5a16a57d855eb71a25c5b29eaff09
klaymanlei <klay.lei@aliyun.com>
2018-02-12 02:52:47 +0800

    create separate flask config file

---
+++ config_flask.py
@@ -0,0 +1,5 @@
+#coding:utf-8
+
+DEBUG = True
+#HOST = '0.0.0.0'
+#PORT = 5000
--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+import config_flask
 
 app = Flask(__name__)
+app.config.from_object(config_flask)
 
 @app.route('/')
 def hello_world():
