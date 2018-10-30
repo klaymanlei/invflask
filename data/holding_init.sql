8fdc41b39d8ca1d41fe2fbe96d05c9cee381a783
dayu <klay.lei@aliyun.com>
2018-10-30 19:34:50 +0800

    add db init sql file

---
+++ data/holding_init.sql
@@ -0,0 +1,12 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.1.73 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `t_holding` (`dt`, `portfolio`, `code`, `sec_type`, `quantity`, `amount`) values('2016-01-01','freedom','-','cash','100000.0000','100000.0000');
+insert into `t_holding` (`dt`, `portfolio`, `code`, `sec_type`, `quantity`, `amount`) values('2016-01-01','other','-','cash','0.0000','0.0000');
+insert into `t_holding` (`dt`, `portfolio`, `code`, `sec_type`, `quantity`, `amount`) values('2016-01-01','oth_funds','-','cash','300000.0000','300000.0000');
+insert into `t_holding` (`dt`, `portfolio`, `code`, `sec_type`, `quantity`, `amount`) values('2016-01-01','ss50_fund','-','cash','150000.0000','150000.0000');
+insert into `t_holding` (`dt`, `portfolio`, `code`, `sec_type`, `quantity`, `amount`) values('2016-01-01','ss50_lowestpb','-','cash','500000.0000','500000.0000');
